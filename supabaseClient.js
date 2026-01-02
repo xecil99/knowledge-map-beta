1cbdbbcc0374bd0c8b919d49cfc1ac34ea4344e4
xecil99 <xecil99@gmail.com>
2026-01-02 00:33:53 -0800

    Fix supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 // supabaseClient.js
 export function createSupabaseClient() {
-  const url = "https://xecil99.github.io/knowledge-map-beta/";
+  const url = "https://zweebefyolhubnspihzk.supabase.co";
   const anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3ZWViZWZ5b2xodWJuc3BpaHprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMjAxMDIsImV4cCI6MjA4MjY5NjEwMn0.wXH218qQye5ZPncCQ03tZlijEQwKJNNYSvCvaFPt0EA";
 
   return window.supabase.createClient(url, anonKey, {
