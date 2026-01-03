c6dd4a51ccb40d50ab30440fab5d67645b4f3b8e
xecil99 <xecil99@gmail.com>
2026-01-02 17:19:35 -0800

    Another typo type thing

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,6 @@
 // supabaseClient.js
 export function createSupabaseClient() {
   const url = "https://zweebefyolhubnspihzk.supabase.co";
-  https://zweebefyolhubnspihzk.supabase.co/auth/v1/callback
   const anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3ZWViZWZ5b2xodWJuc3BpaHprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMjAxMDIsImV4cCI6MjA4MjY5NjEwMn0.wXH218qQye5ZPncCQ03tZlijEQwKJNNYSvCvaFPt0EA";
 
   return window.supabase.createClient(url, anonKey, {
