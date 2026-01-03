fbf9463c019ecbfde945bf4ba5282d8265e4ba79
xecil99 <xecil99@gmail.com>
2026-01-02 17:01:03 -0800

    Some typo or something

--- supabaseClient.js
+++ supabaseClient.js
@@ -4,7 +4,7 @@ export function createSupabaseClient() {
   const anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3ZWViZWZ5b2xodWJuc3BpaHprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMjAxMDIsImV4cCI6MjA4MjY5NjEwMn0.wXH218qQye5ZPncCQ03tZlijEQwKJNNYSvCvaFPt0EA";
 
   return window.supabase.createClient(url, anonKey, {
-    auth: {
+    auth: {a
       flowType: "pkce",
       detectSessionInUrl: true,
       persistSession: true,
